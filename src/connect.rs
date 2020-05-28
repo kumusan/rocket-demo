38d669347661c220325e06d721b71d0beb3e378f
kumusan <ryogi0200@gmail.com>
2020-05-29 02:11:48 +0900

    [delete]🛠dotenv

--- src/connect.rs
+++ src/connect.rs
@@ -1,11 +1,7 @@
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
 
-use dotenv::dotenv;
-
 pub fn connect_init() -> PgConnection {
-    dotenv().ok();
-
     let database_url = "postgres://postgres:postgres@localhost:5433";
     PgConnection::establish(&database_url)
         .expect(&format!("Error connecting to {}", database_url))
