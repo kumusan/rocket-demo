660aab3375ee7177cf717a7fca08e1fac07ab33b
kumusan <ryogi0200@gmail.com>
2020-06-01 01:02:41 +0900

    [test]🔍query_test

--- src/query.rs
+++ src/query.rs
@@ -1,28 +1,56 @@
-use diesel;
-use diesel::prelude::*;
-use models::Todo;
-use schema::*;
+use diesel::*;
+use crate::models::*;
+// use crate::schema::todos;
+use crate::connect::connect_init;
+#[cfg(test)]
+use diesel::debug_query;
+// use diesel::select;
 
-pub fn all(connection: &PgConnection) -> QueryResult<Vec<Todo>> {
-    schema::diesel::todos::table.load::<Todo>(&*connection)
+mod schema {
+    table! {
+    pub todos (id) {
+        id -> Int4,
+        title -> Varchar,
+        body -> Text,
+        done -> Bool,
+    }
+}
 }
 
-pub fn get(id: i32, connection: &PgConnection) -> QueryResult<Todo> {
-    todos::table.find(id).get_result::<Todo>(connection)
+#[test]
+pub fn query_test() { //-> Result<QueryResult<Vec<Todo>>, Err> {
+    // todos::table.load::<Todo>(&*connection)
+    use schema::todos::dsl::*;
+    let connection = connect_init();
+    connection.execute("SELECT * FROM todos").unwrap();
+    assert_eq!(
+        Ok(Todo::new(1, " new title", "new text")), 
+        todos.find(1).first(&connection));
 }
+// pub fn all(connection: &PgConnection) {
+//     let results = todos
+//         .filter(done.eq(true))
+//         .limit(5)
+//         .load::<Todo>(&connection)
+//         .expect("Err all");
+// }
 
-pub fn insert(Todo: Todo, connection: &PgConnection) -> QueryResult<Todo> {
-    diesel::insert_into(todos::table)
-        .values(&InsertableTodo::from_Todo(Todo))
-        .get_result(connection)
-}
+// pub fn get(id: i32, connection: &PgConnection) -> QueryResult<Todo> {
+//     connection.execute("SELECT * FROM todos WHERE ")
+// }
 
-pub fn update(id: i32, Todo: Todo, connection: &PgConnection) -> QueryResult<Todo> {
-    diesel::update(todos::table.find(id))
-        .set(&Todo)
-        .get_result(connection)
-}
+// pub fn insert(Todo: Todo, connection: &PgConnection) -> QueryResult<Todo> {
+//     diesel::insert_into(todos::table)
+//         .values(&Todo::from_todo(Todo))
+//         .get_result(connection)
+// }
 
-pub fn delete(id: i32, connection: &PgConnection) -> QueryResult<usize> {
-    diesel::delete(todos::table.find(id)).execute(connection)
-}
+// pub fn update(id: i32, Todo: Todo, connection: &PgConnection) -> QueryResult<Todo> {
+//     diesel::update(todos::table.find(id))
+//         .set(&Todo)
+//         .get_result(connection)
+// }
+
+// pub fn delete(id: i32, connection: &PgConnection) -> QueryResult<usize> {
+//     diesel::delete(todos::table.find(id)).execute(connection)
+// }
