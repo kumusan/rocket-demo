a757f0e041158ca4b9b17fc86e9f6cd128a6dbe5
kumusan <ryogi0200@gmail.com>
2020-06-02 02:35:59 +0900

    [add]🎇pg_schema_include

--- src/models.rs
+++ src/models.rs
@@ -1,22 +1,31 @@
-use serde::{Deserialize, Serialize};
+// use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+// #[derive(Debug, Serialize, Deserialize, Queryable)]
+use diesel::*;
+use super::schema::todos;
+
+#[cfg(feature = "postgres")]
+include!("schema.rs");
+
+#[derive(
+    PartialEq, Eq, Debug, Clone, Queryable, Identifiable, Insertable, AsChangeset, QueryableByName,
+)]
+#[table_name = "todos"]
 pub struct Todo {
-  pub id: u32,
+  pub id: i32,
   pub title: String,
   pub body: String,
   pub done: bool,
 }
 
 impl Todo {
-  pub fn new(id: u32, title: &str, body: &str) -> Self {
+  pub fn new(id: i32, title: &str, body: &str, done: bool) -> Self {
     Todo {
         id: id,
         title: title.to_string(),
         body: body.to_string(),
-        done: false,
+        done: done,
     }
-
   }
   fn from_todo(todo: Todo) -> Todo {
     Todo {
