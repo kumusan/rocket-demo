ecf8c478c161b92115ecfb50a1032bccf8cd1d06
kumusan <ryogi0200@gmail.com>
2020-06-02 18:03:02 +0900

    [add]🛠select_test

--- src/query.rs
+++ src/query.rs
@@ -8,38 +8,57 @@ use diesel::insert_into;
 use diesel::pg::Pg;
 
 #[test]
-pub fn insert_test() {
+pub fn insert_query_test() {
     let query = insert_into(todos).default_values();
     let sql = "INSERT INTO \"todos\" DEFAULT VALUES -- binds: []";
     assert_eq!(sql, debug_query::<Pg, _>(&query).to_string());
 }
 
 #[test]
-pub fn get_all() { //-> Result<QueryResult<Vec<Todo>>, Err> {
+pub fn get_all() { 
     let connection = connect_init();
-    insert_into(todos).values(Todo::new(1, "new title", "new text", false)).execute(&connection).unwrap();
+    insert_into(todos)
+        .values(
+            vec![
+                Todo::new(1, "new title", "new text", false), 
+                Todo::new(2, "test2", "test2", false)
+            ]
+        )
+        .execute(&connection).unwrap();
     // ローカルならErrが返ってくる
     let db_load = todos.load::<Todo>(&connection).unwrap();
-    let test_todo = vec![Todo { id: 1, title: "new title".to_string(), body: "new text".to_string(), done: false }];
+    let test_todo = vec![
+        Todo {id: 1, title: "new title".to_string(), body: "new text".to_string(), done: false},
+        Todo {id: 2, title: "test2".to_string(), body: "test2".to_string(), done: false},
+    ];
     assert_eq!(test_todo, db_load);
 }
-// pub fn all(connection: &PgConnection) {
-//     let results = todos
-//         .filter(done.eq(true))
-//         .limit(5)
-//         .load::<Todo>(&connection)
-//         .expect("Err all");
-// }
+
+#[test]
+pub fn select_test() {
+    let connection = connect_init();
+    insert_into(todos)
+        .values(
+            vec![
+                Todo::new(1, "new title", "new text", false), 
+                Todo::new(2, "test2", "test2", false)
+            ]
+        )
+        .execute(&connection).unwrap();
+    let select = todos.select((id, title, body, done));
+    let db_load = select.load(&connection).unwrap();
+    let test_todo = vec![Todo {id: 1, title: "new title".to_string(), body: "new text".to_string(), done: false}];
+    assert_eq!(test_todo, db_load);
+}
 
 // pub fn get(id: i32, connection: &PgConnection) -> QueryResult<Todo> {
 //     connection.execute("SELECT * FROM todos WHERE ")
 // }
 
-// pub fn insert(Todo: Todo, connection: &PgConnection) -> QueryResult<Todo> {
-//     diesel::insert_into(todos::table)
-//         .values(&Todo::from_todo(Todo))
-//         .get_result(connection)
-// }
+pub fn insert(todo: Todo) {
+    let connection = connect_init();
+    insert_into(todos).values(todo).execute(&connection);
+}
 
 // pub fn update(id: i32, Todo: Todo, connection: &PgConnection) -> QueryResult<Todo> {
 //     diesel::update(todos::table.find(id))
