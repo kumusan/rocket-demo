6ea67236789379776ccd3ddaebaef97f8dd95f7e
kumusan <ryogi0200@gmail.com>
2020-06-07 01:14:55 +0900

    [add]🎇query_create_read

--- src/query.rs
+++ src/query.rs
@@ -2,21 +2,50 @@ use diesel::*;
 use crate::models::*;
 use crate::schema::todos::dsl::*;
 use crate::connect::connect_init;
-#[cfg(test)]
-use diesel::debug_query;
-use diesel::insert_into;
-use diesel::delete;
 use diesel::pg::Pg;
 
+pub fn all() -> Vec<Todo> {
+    let connection = connect_init();
+    todos.load::<Todo>(&connection).unwrap()
+}
+
+pub fn get(todoid: usize) -> Todo {
+    let connection = connect_init();
+    let db_load = todos.load::<Todo>(&connection).unwrap();
+    let todoid = todoid - 1;
+    db_load[todoid].clone()
+}
+
+pub fn create(todo: Todo) {
+    let connection = connect_init();
+    insert_into(todos).values(todo).execute(&connection).unwrap();
+}
+
+// pub fn update(id: i32, Todo: Todo, connection: &PgConnection) -> QueryResult<Todo> {
+//     diesel::update(todos::table.find(id))
+//         .set(&Todo)
+//         .get_result(connection)
+// }
+
+// pub fn delete(id: i32, connection: &PgConnection) -> QueryResult<usize> {
+//     diesel::delete(todos::table.find(id)).execute(connection)
+// }
+
 #[test]
-pub fn insert_query_test() {
+fn query() {
+    insert_query_test();
+    get_all();
+    select_test();
+    delete_test();
+}
+
+fn insert_query_test() {
     let query = insert_into(todos).default_values();
     let sql = "INSERT INTO \"todos\" DEFAULT VALUES -- binds: []";
     assert_eq!(sql, debug_query::<Pg, _>(&query).to_string());
 }
 
-#[test]
-pub fn get_all() { 
+fn get_all() { 
     let connection = connect_init();
     insert_into(todos)
         .values(
@@ -35,17 +64,15 @@ pub fn get_all() {
     assert_eq!(test_todo, db_load);
 }
 
-#[test]
-pub fn select_test() {
+fn select_test() {
     let connection = connect_init();
-    let db_load = todos.filter(id.eq(1)).load(&connection).unwrap(); // return []
+    let db_load = todos.filter(id.eq(1)).load(&connection).unwrap();
     let test_todo = vec![Todo {id: 1, title: "new title".to_string(), body: "new text".to_string(), done: false}];
     assert_eq!(test_todo, db_load, "db_load is []");
     let test_todo = Todo::new(1, "new title", "new text", false);
     assert_eq!(Ok(test_todo), todos.filter(id.eq(1)).first(&connection));
 }
 
-#[test]
 fn delete_test() {
     let connection = connect_init();
     let db_load = todos.load::<Todo>(&connection).unwrap();
@@ -53,23 +80,7 @@ fn delete_test() {
     let id_2 = db_load[1].clone();
     delete(&id_2).execute(&connection).unwrap();
     assert_eq!(Ok(vec![id_1]), todos.load(&connection));
+    let db_load = todos.load::<Todo>(&connection).unwrap();
+    let id_1 = db_load[0].clone();
+    delete(&id_1).execute(&connection).unwrap();
 }
-
-// pub fn get(id: i32, connection: &PgConnection) -> QueryResult<Todo> {
-//     connection.execute("SELECT * FROM todos WHERE ")
-// }
-
-pub fn insert(todo: Todo) {
-    let connection = connect_init();
-    insert_into(todos).values(todo).execute(&connection).unwrap();
-}
-
-// pub fn update(id: i32, Todo: Todo, connection: &PgConnection) -> QueryResult<Todo> {
-//     diesel::update(todos::table.find(id))
-//         .set(&Todo)
-//         .get_result(connection)
-// }
-
-// pub fn delete(id: i32, connection: &PgConnection) -> QueryResult<usize> {
-//     diesel::delete(todos::table.find(id)).execute(connection)
-// }
