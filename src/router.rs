e26ad4d82984d0550a2f980b809609c29090f254
kumusan <ryogi0200@gmail.com>
2020-06-07 03:40:29 +0900

    [add]🎈query_update_delete

--- src/router.rs
+++ src/router.rs
@@ -4,33 +4,49 @@ use rocket_contrib::json::Json;
 
 #[get("/")]
 pub fn index() -> &'static str {
-  "hello wasm rocket!"
+    "hello wasm rocket!"
 }
 
 #[get("/todos")]
 pub fn all_todos() -> Json<Vec<Todo>> {
-  Json(all())
+    Json(all())
 }
 
 #[post("/todos", data = "<todo>")]
 pub fn new_todo(todo: Json<Todo>) -> String {
-  let todo: Todo = Todo{
-    id: todo.id,
-    title: todo.title.to_string(),
-    body: todo.body.to_string(),
-    done: todo.done,
-  };
-  create(todo);
-  format!("ok")
+    let todo: Todo = Todo{
+        id: todo.id,
+        title: todo.title.to_string(),
+        body: todo.body.to_string(),
+        done: todo.done,
+    };
+    create(todo);
+    format!("ok")
 }
 
-#[get("/todo/<todoid>")]
-pub fn get_id(todoid: usize) -> Json<Vec<Todo>> {
-  let result = get(todoid);
-  Json(vec![result])
+#[get("/todos/<todoid>")]
+pub fn get_id(todoid: i32) -> Json<Vec<Todo>> {
+    let result = get(todoid);
+    Json(result)
+}
+
+#[post("/todos/<todoid>", data = "<todo>")]
+pub fn update_id(todoid: i32, todo: Json<Todo>) {
+    let todo: Todo = Todo{
+        id: todo.id,
+        title: todo.title.to_string(),
+        body: todo.body.to_string(),
+        done: todo.done,
+    };
+    update(todoid, todo);
+}
+
+#[delete("/todos/<todoid>")]
+pub fn delete_id(todoid: i32) {
+    delete(todoid);
 }
 
 #[test]
 fn route_test() {
-  assert_eq!(index(), "hello wasm rocket!");
+    assert_eq!(index(), "hello wasm rocket!");
 }
